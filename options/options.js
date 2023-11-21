0cb24036c99f8606dee67f0a25845b88bcb0a834
Crowdbotics <team@crowdbotics.com>
2023-11-21 02:13:23 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "papaa-marketplace-a-44697",
+  url: "https://papaa-marketplace-a-44697.botics.co",
+  api: "https://papaa-marketplace-a-44697.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
